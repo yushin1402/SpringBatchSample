18c701f50584c3adec62e5de7aaeb33858b0d22c
yushin1402 <s.masaomi16h@gmail.com>
2021-11-22 01:15:10 +0900

    first commit

---
+++ src/test/java/com/example/batch/SpringBatchSample/SpringBatchSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.batch.SpringBatchSample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
